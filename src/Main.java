b82d4550f650492fd87b218d2ce186b5cbdc5d6f
Loredana <lmtnvc@gmail.com>
2025-04-03 21:09:34 +0200

    3.4. main branch

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ public class Main {
 
     public static void main(String... args) {
 
-            Krug krug;
+            Krug krug = new Krug("jedankrug",6);
+            Krug krug1 = new Krug("drugikruig", 37.1);
+        Pravokutnik pravokutnik1 = new Pravokutnik ("jedanPravokutnik", 7.6, 97);
+        Trokut trokut = new Trokut("jedantrokut",10, 5, 8);
+
 
     }
 }
